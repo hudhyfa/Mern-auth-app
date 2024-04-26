d155d3a3ac17a1535c54108b81c241ebb4ca1145
hudhyfa <hudhyfahbk@gmail.com>
2024-04-26 21:21:24 +0530

    created and connected database to the server

---
+++ server/index.js
@@ -0,0 +1,18 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("connected to database");
+  })
+  .catch((err) => {
+    console.log("error connecting to database: " + err.message);
+  });
+
+const app = express();
+app.listen(3000, () => {
+  console.log("Server listening on port 3000 !!!");
+});
