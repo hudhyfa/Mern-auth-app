b205d85d5eb3be61f7c926465fb3a9aa4f9ab0ce
hudhyfa <hudhyfahbk@gmail.com>
2024-04-26 17:37:38 +0530

    Added routes and their corresponding pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Profile from './pages/Profile';
+import Signup from './pages/Signup';
+import Signin from './pages/Signin';
 
 const App = () => {
   return (
-    <div>
-      <h2>hello im hudyfa</h2>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={ <Home /> } />
+        <Route path='/about' element={ <About /> } />
+        <Route path='/profile' element={ <Profile /> } />
+        <Route path='/sign-up' element={ <Signup /> } />
+        <Route path='/sign-in' element={ <Signin /> } />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
