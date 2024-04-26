90ab34a4aa2e09bb6458c8b3ee9a394ffc13d58f
hudhyfa <hudhyfahbk@gmail.com>
2024-04-26 17:59:15 +0530

    added header component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,10 +5,12 @@ import About from './pages/About';
 import Profile from './pages/Profile';
 import Signup from './pages/Signup';
 import Signin from './pages/Signin';
+import Header from './components/Header';
 
 const App = () => {
   return (
     <BrowserRouter>
+    <Header />
       <Routes>
         <Route path='/' element={ <Home /> } />
         <Route path='/about' element={ <About /> } />
