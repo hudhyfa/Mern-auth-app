a4745b81515145105b7e374d8df21c55bba5cca4
hudhyfa <hudhyfahbk@gmail.com>
2024-04-29 20:11:25 +0530

    added sigin page logic and UI.

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 function Signup() {
   const [formData, setFormData] = useState({});
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
+  const navigate = useNavigate();
 
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.id]: e.target.value });
@@ -23,10 +24,11 @@ function Signup() {
       });
       const data = await res.json();
       setLoading(false);
-      if (!data.success) {
+      if (data.success === false) {
         setError(true);
         return;
       }
+      navigate('/sign-in')
     } catch (error) {
       setLoading(false);
       setError(true);
