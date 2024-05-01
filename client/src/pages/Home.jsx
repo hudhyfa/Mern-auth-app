a0864a1b695dc882091e80323a86594c56531c6f
hudhyfa <hudhyfahbk@gmail.com>
2024-05-01 17:39:14 +0530

    completed home and about pages

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from "react";
 
 function Home() {
   return (
-    <div>Home</div>
-  )
+    <div className="px-4 py-12 max-w-2xl mx-auto">
+      <h1 className="text-3xl font-bold mb-4 text-slate-800">Welcome to my auth app!</h1>
+      <p className="mb-4 text-slate-700 ">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio at quidem
+        laborum eos nemo officiis maxime minus sunt, ut libero expedita cum
+        distinctio reprehenderit, error ea sequi molestias dolorum, eum vel!
+        Voluptate repellat rerum at perspiciatis harum porro accusantium beatae
+      </p>
+      <p className="mb-4 text-slate-700 ">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio at quidem
+        laborum eos nemo officiis maxime minus sunt, ut libero expedita cum
+        distinctio reprehenderit, error ea sequi molestias dolorum, eum vel!
+        Voluptate repellat rerum at perspiciatis harum porro accusantium beatae
+      </p>
+      <p className="mb-4 text-slate-700 ">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio at quidem
+        laborum eos nemo officiis maxime minus sunt, ut libero expedita cum
+        distinctio reprehenderit, error ea sequi molestias dolorum, eum vel!
+        Voluptate repellat rerum at perspiciatis harum porro accusantium beatae
+      </p>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
