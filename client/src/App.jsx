32ddd895bab11d97a04f471d5fab5544c65189ff
hudhyfa <hudhyfahbk@gmail.com>
2024-05-01 06:40:03 +0530

    added Private profile to header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import Profile from './pages/Profile';
 import Signup from './pages/Signup';
 import Signin from './pages/Signin';
 import Header from './components/Header';
+import PrivateRoute from './components/PrivateRoute';
 
 const App = () => {
   return (
@@ -14,7 +15,9 @@ const App = () => {
       <Routes>
         <Route path='/' element={ <Home /> } />
         <Route path='/about' element={ <About /> } />
-        <Route path='/profile' element={ <Profile /> } />
+        <Route element={ <PrivateRoute /> } >
+          <Route path='/profile' element={ <Profile /> } />
+        </Route>
         <Route path='/sign-up' element={ <Signup /> } />
         <Route path='/sign-in' element={ <Signin /> } />
       </Routes>
